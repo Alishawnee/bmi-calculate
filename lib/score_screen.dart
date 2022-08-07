3b2b890a06bdf31690a8b24b55633c079f0e88fe
unknown <alishawnee98@gmail.com>
2022-08-07 08:22:26 +0300

    initial

---
+++ lib/score_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:pretty_gauge/pretty_gauge.dart';
+import 'package:share_plus/share_plus.dart';
+
+class ScoreScreen extends StatelessWidget {
+  final double bmiScore;
+
+  final int age;
+
+  String? bmiStatus;
+
+  String? bmiInterpretation;
+
+  Color? bmiStatusColor;
+
+  ScoreScreen({Key? key, required this.bmiScore, required this.age})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    setBmiInterpretation();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("النتيجة"),
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(12),
+          child: Card(
+              elevation: 12,
+              shape: const RoundedRectangleBorder(),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "وزنك المثالي",
+                      style: TextStyle(fontSize: 30, color: Colors.blue),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    PrettyGauge(
+                      gaugeSize: 300,
+                      minValue: 0,
+                      maxValue: 40,
+                      segments: [
+                        GaugeSegment('UnderWeight', 18.5, Colors.red),
+                        GaugeSegment('Normal', 6.4, Colors.green),
+                        GaugeSegment('OverWeight', 5, Colors.orange),
+                        GaugeSegment('Obese', 10.1, Colors.pink),
+                      ],
+                      valueWidget: Text(
+                        bmiScore.toStringAsFixed(1),
+                        style: const TextStyle(fontSize: 40),
+                      ),
+                      currentValue: bmiScore.toDouble(),
+                      needleColor: Colors.blue,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      bmiStatus!,
+                      style: TextStyle(fontSize: 20, color: bmiStatusColor!),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      bmiInterpretation!,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text("اعادة الحساب")),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              Share.share(
+                                  "Your BMI is ${bmiScore.toStringAsFixed(1)} at age $age");
+                            },
+                            child: const Text("مشاركة")),
+                      ],
+                    )
+                  ]))),
+    );
+  }
+
+  void setBmiInterpretation() {
+    if (bmiScore > 30) {
+      bmiStatus = "سمين";
+      bmiInterpretation = "الرجاء العمل على تقليل السمنة";
+      bmiStatusColor = Colors.pink;
+    } else if (bmiScore >= 25) {
+      bmiStatus = "زيادة في وزنك";
+      bmiInterpretation = "ممارسة التمارين الرياضية بانتظام وتقليل الوزن";
+      bmiStatusColor = Colors.orange;
+    } else if (bmiScore >= 18.5) {
+      bmiStatus = "طبيعي";
+      bmiInterpretation = "استمتع ، أنت لائق";
+      bmiStatusColor = Colors.green;
+    } else if (bmiScore < 18.5) {
+      bmiStatus = "وزنك منخفض";
+      bmiInterpretation = "حاول زيادة الوزن";
+      bmiStatusColor = Colors.red;
+    }
+  }
+}
